fa50517d1acfb3307d6780d47cac48d5733d2ac4
NikhilMunda <46515697+NikhilMunda@users.noreply.github.com>
2024-09-06 13:32:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 window.onload = function() {
     // Hide the loading screen
     document.getElementById('loading-screen').style.display = 'none';
+    document.getElementById('main-content').style.display = 'block';
 };
         
 
